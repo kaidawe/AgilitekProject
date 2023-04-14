763321f34cc461466f56d64ab51d31c7ca3cb187
Renz <rgabrinao1@gmail.com>
2023-04-14 12:30:50 -0700

    test tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Agilitek"
+  }, []);
+
   return (
     <div className="text-4xl text-stone-700 text-center">
       <h1>Hello World</h1>
