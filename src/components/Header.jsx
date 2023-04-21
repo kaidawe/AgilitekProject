fde631adb0cc89fd0a6b09fdc40f077f23168808
Haley Dawe <hdawe@my.bcit.ca>
2023-04-21 14:45:57 -0700

    fixed class name issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,17 @@ import '../styles/Header.css';
 
 export default function Header() {
   return (
-    <nav class="header">
-      <ul class="headerItems">
+    <nav className="header">
+      <ul className="headerItems">
         <li>
           <Link to="/admin">
-          <i class="fa-solid fa-square-poll-horizontal"></i><br></br>
+          <i className="fa-solid fa-square-poll-horizontal"></i><br></br>
             Admin
           </Link>
         </li>
         <li>
           <Link to="/">
-          <i class="fa-solid fa-square-poll-horizontal"></i><br></br>
+          <i className="fa-solid fa-square-poll-horizontal"></i><br></br>
             User
           </Link>
         </li>
