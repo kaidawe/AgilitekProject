a12615af2343e7664ffbb93a37f2aa1343394512
Tony Kieling <tony.kieling@gmail.com>
2023-04-21 13:01:36 -0700

    adding temporary json file so we can start the FE app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+// import data from "../data/json-file-old.json";  // this file contains step_history column (not sending now bkz it is 100MB!)
+
+import data from "../data/json-file.json";  
+// this file gets ride of the step_history and 
+// creates a new prop called errorMsg (when the run_status is "failed")
 
 function App() {
   const [items, setItems] = useState([]);
@@ -14,11 +19,17 @@ function App() {
         console.log(JSON.parse(data));
         setItems(JSON.parse(data))
       });
-      
     }
+    // console.log("data= ", data[0]);
+    setItems(data);
     document.title = "Agilitek";
-    testPull();
+    // testPull();
   }, []);
+
+  // temporary thing to display on console the data
+  useEffect(() => {
+    console.log("items: ", items);
+  }, [items]);
 
   return (
     <div className=" text-stone-700 text-center">
@@ -30,19 +41,21 @@ function App() {
             <th>Log Details</th>
             <th>CLS</th>
             <th>Status</th>
+            <th>Run Status</th>
+            <th>Error Msg</th>
           </tr>
         </thead>
         <tbody>
-          {items.map(x => (
-            <>
-              <tr>
-                <td>{x.pk}</td>
-                <td>{x.id}</td>
-                <td>{x.log_details}</td>
-                <td>{x.cls}</td>
-                <td>{x.run_status}</td>
-              </tr>
-            </>
+          {items.map((x, index) => (
+            <tr key={index}>
+            <td>{index}</td>
+            <td>{x.pk}</td>
+            <td>{x.id}</td>
+            <td>{x.log_details}</td>
+            <td>{x.cls}</td>
+            <td>{x.run_status}</td>
+            <td>{x.errorMsg}</td>
+            </tr>
           ))}
         </tbody>
       </table>
